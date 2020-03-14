ce330cd627505635c7c8c1c2beace4a763a0f201
CharithBK <32832295+CharithBK@users.noreply.github.com>
2020-03-14 21:53:27 +0530

    new update

---
+++ lib/pages/product_details.dart
@@ -0,0 +1,254 @@
+import 'package:flutter/material.dart';
+
+class ProductDetails extends StatefulWidget {
+  final product_detail_name;
+  final product_detail_new_price;
+  final product_detail_old_price;
+  final product_detail_picture;
+
+  ProductDetails(
+      {this.product_detail_name,
+      this.product_detail_new_price,
+      this.product_detail_old_price,
+      this.product_detail_picture});
+
+  @override
+  _ProductDetailsState createState() => _ProductDetailsState();
+}
+
+class _ProductDetailsState extends State<ProductDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        elevation: 0.1,
+        backgroundColor: Colors.red,
+        title: Text('Fashapp'),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {})
+        ],
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new Container(
+            height: 300.0,
+            child: GridTile(
+              child: Container(
+                color: Colors.white,
+                child: Image.asset(widget.product_detail_picture),
+              ),
+              footer: new Container(
+                  color: Colors.white70,
+                  child: ListTile(
+                    leading: new Text(
+                      widget.product_detail_name,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16.0),
+                    ),
+                    title: new Row(children: <Widget>[
+                      Expanded(
+                          child: new Text(
+                        "\$${widget.product_detail_old_price}",
+                        style: TextStyle(
+                            color: Colors.grey,
+                            decoration: TextDecoration.lineThrough),
+                      )),
+                      Expanded(
+                          child: new Text(
+                              "\$${widget.product_detail_new_price}",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red)))
+                    ]),
+                  )),
+            ),
+          ),
+
+          //btn1==========
+          Row(
+            children: <Widget>[
+              //btnSize=============================
+
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: new Text("Size"),
+                            content: new Text("Choose the size"),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: new Text("Close"),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: new Text("Size")),
+                      Expanded(child: new Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+              //btnColor=============================
+
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: new Text("Color"),
+                            content: new Text("Choose the color"),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: new Text("Close"),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: new Text("Color")),
+                      Expanded(child: new Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+
+              //btnQty=============================
+
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: new Text("Quantity"),
+                            content: new Text("Choose the quantity"),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: new Text("Close"),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: new Text("Qty")),
+                      Expanded(child: new Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+//btn2======================
+          Row(
+            children: <Widget>[
+              //btn=============================
+
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  elevation: 0.2,
+                  child: new Text("Buy now"),
+                ),
+              ),
+
+              new IconButton(
+                  icon: Icon(
+                    Icons.add_shopping_cart,
+                    color: Colors.red,
+                  ),
+                  onPressed: () {}),
+              new IconButton(
+                  icon: Icon(
+                    Icons.favorite_border,
+                    color: Colors.red,
+                  ),
+                  onPressed: () {}),
+            ],
+          ),
+          Divider(),
+          new ListTile(
+            title: new Text("Product Details"),
+            subtitle: new Text("Jun Takahashi and his GIRA (Gyakusou International Running Association) club are back with the Nike x Gyakusou T-Shirt. It revisits '70s Nike graphics for inspiration while nodding to the GIRA crew's runs in Yoyogi Park."),
+          ),
+          Divider(),
+          new Row(
+            children: <Widget>[
+            Padding(padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+            child: new Text("Product name" , style: TextStyle(color: Colors.grey)),),
+            Padding(padding: EdgeInsets.all(5.0),
+            child: new Text(widget.product_detail_name),
+            )
+          ],
+            
+          ),
+
+           new Row(
+            children: <Widget>[
+            Padding(padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+            child: new Text("Product brand" , style: TextStyle(color: Colors.grey)),),
+             Padding(padding: EdgeInsets.all(5.0),
+            child: new Text("Nike"),
+            )
+          ],
+            
+          ),
+
+           new Row(
+            children: <Widget>[
+            Padding(padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+            child: new Text("Product condition" , style: TextStyle(color: Colors.grey)),),
+             Padding(padding: EdgeInsets.all(5.0),
+            child: new Text("BRAND NEW"),
+            )
+          ],
+            
+          ),
+        ],
+      ),
+    );
+  }
+}
