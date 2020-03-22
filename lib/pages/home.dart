5f885a9e3c517716f590fdf9ff78a389c50d9918
CharithBK <32832295+CharithBK@users.noreply.github.com>
2020-03-22 11:34:37 +0530

    login db

---
+++ lib/pages/home.dart
@@ -0,0 +1,260 @@
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+
+//my own imports
+import 'package:shopapp_tut/componets/horizontal_listview.dart';
+import 'package:shopapp_tut/componets/products.dart';
+import 'package:shopapp_tut/pages/cart.dart';
+
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  get http => null;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget image_carousel = new Container(
+      height: 200.0,
+      child:  new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/w3.jpeg'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/w4.jpeg'),
+          AssetImage('images/m2.jpg'),
+        ],
+        autoplay: true,
+//      animationCurve: Curves.fastOutSlowIn,
+//      animationDuration: Duration(milliseconds: 1000),
+        dotSize: 4.0,
+        indicatorBgPadding: 2.0,
+        dotBgColor: Colors.transparent,
+      ),
+    );
+
+
+    return Scaffold(
+      appBar: new AppBar(
+        elevation: 0.1,
+        backgroundColor: Colors.green,
+        title: Text('ⓉⓇⒺⓃⒹⓈ'),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> new Cart()));
+              })
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+//            header
+            new UserAccountsDrawerHeader(
+              accountName: Text('Chaminda Puswedilla'),
+              accountEmail: Text('trends@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.lime,
+                  child: Icon(Icons.person, color: Colors.white,),
+                ),
+              ),
+              decoration: new BoxDecoration(
+                  color: Colors.green
+              ),
+            ),
+
+//            body
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home ,color: Colors.green,),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My account'),
+                leading: Icon(Icons.person,color: Colors.green,),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My Orders'),
+                leading: Icon(Icons.shopping_basket,color: Colors.green,),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> new Cart()));
+              },
+              child: ListTile(
+                title: Text('Categoris'),
+                leading: Icon(Icons.shopping_cart ,color: Colors.green,),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Favourites'),
+                leading: Icon(Icons.favorite ,color: Colors.green,),
+              ),
+            ),
+
+            Divider(),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+
+
+      body: new Column(
+
+        children: <Widget>[
+          //image carousel begins here
+          // image_carousel,
+
+          //padding widget
+          new Padding(padding: const EdgeInsets.all(4.0),
+            child: Container(
+                alignment: Alignment.centerLeft,
+                child: new Text('Categories')),),
+
+          //Horizontal list view begins here
+          HorizontalList(),
+
+          //padding widget
+          new Padding(padding: const EdgeInsets.all(8.0),
+            child: Container(
+                alignment: Alignment.centerLeft,
+                child: new Text('Recent products')),),
+
+          //grid view
+          Flexible(child: Products()),
+        ],
+      ),
+    );
+  }
+}
+
+
+/*import 'package:chat_app/provider/user_provider.dart';
+import 'package:chat_app/screens/product_details.dart';
+import 'package:chat_app/widgets/custom_app_bar.dart';
+import 'package:chat_app/widgets/featured_products.dart';
+import 'package:chat_app/widgets/product_card.dart';
+import 'package:chat_app/widgets/search.dart';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+//           Custom App bar
+            CustomAppBar(),
+
+//          Search Text field
+            Search(),
+
+//            featured products
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: new Text('Featured products')),
+                ),
+              ],
+            ),
+            FeaturedProducts(),
+
+//          recent products
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: new Text('Recent products')),
+                ),
+              ],
+            ),
+            ProductCard(
+              brand: 'SantosBrand',
+              name: 'Lux Blazer',
+              price: 24.00,
+              onSale: true,
+              picture: '',
+            ),
+            ProductCard(
+              brand: 'SantosBrand',
+              name: 'Lux Blazer',
+              price: 24.00,
+              onSale: true,
+              picture: '',
+            ),
+            ProductCard(
+              brand: 'SantosBrand',
+              name: 'Lux Blazer',
+              price: 24.00,
+              onSale: true,
+              picture: '',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}*/
---
+++ lib/pages/login.dart
@@ -0,0 +1,368 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shopapp_tut/pages/home.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  TextEditingController user=new TextEditingController();
+  TextEditingController pass=new TextEditingController();
+
+  String msg='';
+
+  // ignore: missing_return
+  Future<List> _login() async {
+    final response = await http.post(
+        "https://etrendsapp.000webhostapp.com/getData.php", body: {
+      "username": user.text,
+      "password": pass.text,
+    });
+
+    var datauser = json.decode(response.body);
+
+    if (datauser.length == 0) {
+      setState(() {
+        msg = "Login Fail";
+      });
+    } else {
+      if (datauser[0]['level'] == 'admin') {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()),);
+      }
+      setState(() {
+        var username = datauser[0]['username'];
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    final username = TextField(
+      controller: user,
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "UserName",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final passwordField = TextField(
+      controller: pass,
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()),);},
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+    // ignore: non_constant_identifier_names
+    final RegistrationButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {  _login();  /*Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()),); */},
+        child: Text("Registration",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 155.0,
+                  child: Image.asset(
+                    "assets/logo.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                SizedBox(height: 45.0),
+                username,
+                SizedBox(height: 25.0),
+                passwordField,
+                SizedBox(
+                  height: 35.0,
+                ),
+                loginButon,
+                SizedBox(
+                  height: 15.0,
+                ),
+                RegistrationButton,
+                SizedBox(
+                  height: 15.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+
+    );
+
+  }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+/*import 'package:chat_app/screens/signup.dart';
+import 'package:chat_app/widgets/common.dart';
+import 'package:chat_app/widgets/loading.dart';
+
+import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/user_provider.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  final _key = GlobalKey<ScaffoldState>();
+
+  TextEditingController _email = TextEditingController();
+  TextEditingController _password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context);
+    return Scaffold(
+      key: _key,
+      body: user.status == Status.Authenticating
+          ? Loading()
+          : Stack(
+              children: <Widget>[
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey[350],
+                            blurRadius:
+                                20.0, // has the effect of softening the shadow
+                          )
+                        ],
+                      ),
+                      child: Form(
+                          key: _formKey,
+                          child: ListView(
+                            children: <Widget>[
+                              SizedBox(
+                                height: 40,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Container(
+                                    alignment: Alignment.topCenter,
+                                    child: SvgPicture.asset(
+                                      'images/login.svg',
+                                      width: 260.0,
+                                    )),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                    14.0, 8.0, 14.0, 8.0),
+                                child: Material(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  color: Colors.grey.withOpacity(0.2),
+                                  elevation: 0.0,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 12.0),
+                                    child: TextFormField(
+                                      controller: _email,
+                                      decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Email",
+                                        icon: Icon(Icons.alternate_email),
+                                      ),
+                                      validator: (value) {
+                                        if (value.isEmpty) {
+                                          Pattern pattern =
+                                              r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                                          RegExp regex = new RegExp(pattern);
+                                          if (!regex.hasMatch(value))
+                                            return 'Please make sure your email address is valid';
+                                          else
+                                            return null;
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                    14.0, 8.0, 14.0, 8.0),
+                                child: Material(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  color: Colors.grey.withOpacity(0.2),
+                                  elevation: 0.0,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 12.0),
+                                    child: TextFormField(
+                                      controller: _password,
+                                      decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Password",
+                                        icon: Icon(Icons.lock_outline),
+                                      ),
+                                      validator: (value) {
+                                        if (value.isEmpty) {
+                                          return "The password field cannot be empty";
+                                        } else if (value.length < 6) {
+                                          return "the password has to be at least 6 characters long";
+                                        }
+                                        return null;
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(
+                                    14.0, 8.0, 14.0, 8.0),
+                                child: Material(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                    color: Colors.black,
+                                    elevation: 0.0,
+                                    child: MaterialButton(
+                                      onPressed: () async {
+                                        if (_formKey.currentState.validate()) {
+                                          if (!await user.signIn(
+                                              _email.text, _password.text))
+                                            _key.currentState.showSnackBar(
+                                                SnackBar(
+                                                    content: Text(
+                                                        "Sign in failed")));
+                                        }
+                                      },
+                                      minWidth:
+                                          MediaQuery.of(context).size.width,
+                                      child: Text(
+                                        "Login",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20.0),
+                                      ),
+                                    )),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      "Forgot password",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: black,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: InkWell(
+                                          onTap: () {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        SignUp()));
+                                          },
+                                          child: Text(
+                                            "Create an account",
+                                            textAlign: TextAlign.center,
+                                            style:
+                                                TextStyle(color: Colors.black),
+                                          ))),
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        "or sign in with",
+                                        style: TextStyle(
+                                            fontSize: 18, color: Colors.grey),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: MaterialButton(
+                                          onPressed: () {},
+                                          child: Image.asset(
+                                            "images/ggg.png",
+                                            width: 30,
+                                          )),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          )),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+}*/
+
+
+
